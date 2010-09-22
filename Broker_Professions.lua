3452dce7b247fdd8ed248427db8e5a322c53bd88
durcyn <durcyn@gmail.com>
2010-09-22 14:35:53 -0500

    handle SPELLS_CHANGED by using two numbered primary profession objects

--- Broker_Professions.lua
+++ Broker_Professions.lua
@@ -1,12 +1,14 @@
-﻿local Professions = {}
+local _G = getfenv(0)
+local LibStub = _G.LibStub
+local GetSpellInfo = _G.GetSpellInfo
+local pairs = _G.pairs
+local select = _G.select
 
-local tradeskills = {
+local primary = {
 	(GetSpellInfo(2259)), -- Alchemy
 	(GetSpellInfo(2018)), -- Blacksmithing
-	(GetSpellInfo(2550)), -- Cooking
 	(GetSpellInfo(7411)), -- Enchanting
 	(GetSpellInfo(4036)), -- Engineering
-	(GetSpellInfo(3273)), -- First Aid
 	(GetSpellInfo(25229)), -- Jewelcrafting
 	(GetSpellInfo(2108)), -- Leatherworking
 	(GetSpellInfo(2656)), -- Smelting
@@ -16,20 +18,44 @@ local tradeskills = {
 	(GetSpellInfo(45357)), -- Inscription
 }
 
-local eventFrame = CreateFrame("Frame")
-eventFrame:Hide()
-eventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
-eventFrame:SetScript("OnEvent", function() Professions:Update() end)
+local secondary = {
+	(GetSpellInfo(2550)), -- Cooking
+	(GetSpellInfo(3273)), -- First Aid
+}
 
-function Professions:Update()
-	for k,v in pairs(tradeskills) do
-		if GetSpellInfo(v) then
-			local myicon = select(3, GetSpellInfo(v))
-			Professions[v] = LibStub("LibDataBroker-1.1"):NewDataObject(v,	{
-				type = "launcher",
-				icon = myicon,
-				OnClick = function() CastSpellByName(v) end,
-			})
+local f = CreateFrame("Frame")
+f:Hide()
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
+f:RegisterEvent("SPELLS_CHANGED")
+f:SetScript("OnEvent", function(frame, event, ...) if f.event then f:event(...) end end)
+
+function f:PLAYER_ENTERING_WORLD()
+	for k,v in pairs(secondary) do
+		local icon = select(3, GetSpellInfo(v))
+		if icon then
+			f:create(v, icon)
 		end
 	end
+	f:SPELLS_CHANGED()
+end
+
+function f:SPELLS_CHANGED()
+	local counter = 1
+	for k,v in pairs(primary) do
+		local icon = select(3, GetSpellInfo(v))
+		if icon then
+			f:create(v, icon, counter)
+			counter = counter + 1
+		end
+	end
+end
+
+function f:create(spell, icon, index)
+	local prof = index or spell
+	if not f[prof] then
+		f[prof] = LibStub("LibDataBroker-1.1"):NewDataObject(prof)
+		f[prof].type = "launcher"
+	end
+	f[prof].icon = icon
+	f[prof].OnClick = function() CastSpellByName(spell) end
 end
